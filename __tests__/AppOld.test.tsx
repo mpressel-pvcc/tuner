5475078e7710adc665dd0983aca37bac7966cbf5
Gabriel Fuentes <gffuentes@gmail.com>
2025-05-23 11:21:03 -0700

    feat: starting new home page for v0.1

--- __tests__/AppOld.test.tsx
+++ __tests__/AppOld.test.tsx
@@ -4,10 +4,10 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import AppOld from '../App-old';
 
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+    ReactTestRenderer.create(<AppOld />);
   });
 });
